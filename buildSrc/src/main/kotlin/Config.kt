ee0d3db71a3f6b4f03660919317dfbdaf6e682c9
muazkadan <muaz.kadan@gmail.com>
2025-06-15 21:36:44 +0300

    Remove buildSrc

--- buildSrc/src/main/kotlin/Config.kt
+++
@@ -1,8 +0,0 @@
-object Config {
-    const val compileSdkVersion = 35
-
-    const val minSdkVersion = 21
-    const val targetSdkVersion = 35
-    const val versionCode = 1
-    const val versionName = "0.1"
-}
