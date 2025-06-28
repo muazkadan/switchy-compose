5d8213baba248349a2c781f181afaab9cfc6cb02
muazkadan <muaz.kadan@gmail.com>
2025-06-28 15:17:19 +0300

    Add PhoneFrame composable and update main layout for wasm sample target

--- app/src/wasmJsMain/kotlin/dev/muazkadan/switchycomposedemo/main.kt
+++ app/src/wasmJsMain/kotlin/dev/muazkadan/switchycomposedemo/main.kt
@@ -1,12 +1,22 @@
 package dev.muazkadan.switchycomposedemo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        Box(modifier = Modifier.fillMaxSize().padding(16.dp), contentAlignment = Alignment.Center) {
+            PhoneFrame {
+                App()
+            }
+        }
     }
 }
